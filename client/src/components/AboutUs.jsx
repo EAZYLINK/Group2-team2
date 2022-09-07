7b242072697138f41df3d7dad33244540b0464ae
IkeoluwaAshade <91098490+IkeoluwaAshade@users.noreply.github.com>
2022-09-07 01:37:47 +0100

    first Commit

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -1,8 +1,132 @@
 import React from 'react'
+import './AboutUs.css'
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
+    <div className='mainbody'>
+      
+
+      <div className='section1part1'>
+          
+          <div className='ourMissionHeadTag'>
+           <h5><b>Our Mission</b></h5>
+          </div>
+
+          <div className='ourVisionHeadTag'>
+            <h5><b>Our Vision</b></h5>
+          </div>
+
+        </div>
+
+      <section1 className='ourMission-ourVisionHolder'>
+
+        
+
+        
+
+        <div className='ourMission'>
+
+          <p>
+            We are one of the leading world health  service provider, with the aim of  providing quality health services to   the masses at   affordable rate.
+          </p>
+        </div>
+
+        
+
+        <div className='ourVision'>
+
+          <p>
+            We are one of the leading world health  service provider, with the aim of  providing quality health services to   the masses at   affordable rate.
+          </p>
+        </div>
+      </section1>
+
+
+
+      <section2 className='ourManagementHolder'>
+
+        <div className='section2part1'>
+
+          <div className='centerHeadings'>
+            <h5> <b>Our Management</b></h5>
+          </div>
+
+          
+          <div className='section2CenterParagraph'>
+            <p> Hi guys my name is Ikeoluwa Ashade, I am a software developer who is very very skillfull @ his job. I have worked on over 50 thousand projects.. I am also a Blogger and a Youtuber. To know more about me visit me on legitsub.com/incrediblenetworks.
+            </p>
+          </div>
+
+        </div>
+
+         <div className='section2part2'>
+          <div className='box'>
+          <p>Hi guys my name is Ikeoluwa Ashade, I am a software developer who is very very skillfull @ his job. I have worked on over 50 thousand projects.. I am also a Blogger and a Youtuber. To know more about me visit me on legitsub.com/incrediblenetworks</p>
+          </div>
+
+          <div className='box'>
+          <p>Hi guys my name is Ikeoluwa Ashade, I am a software developer who is very very skillfull @ his job. I have worked on over 50 thousand projects.. I am also a Blogger and a Youtuber. To know more about me visit me on legitsub.com/incrediblenetworks</p>
+          </div>
+
+          <div className='box'>
+          <p>Hi guys my name is Ikeoluwa Ashade, I am a software developer who is very very skillfull @ his job. I have worked on over 50 thousand projects.. I am also a Blogger and a Youtuber. To know more about me visit me on legitsub.com/incrediblenetworks</p>
+          </div>
+
+          <div className='box'>
+          <p>Hi guys my name is Ikeoluwa Ashade, I am a software developer who is very very skillfull @ his job. I have worked on over 50 thousand projects.. I am also a Blogger and a Youtuber. To know more about me visit me on legitsub.com/incrediblenetworks</p>
+          </div>
+
+         
+          
+        </div> 
+
+      </section2>
+
+
+      <section3 className='ourClients'>
+
+        <div className='centerHeadings'><h5><b>Our Client</b></h5></div>
+
+        <p className='section2CenterParagraph'>
+        Hi guys my name is Ikeoluwa Ashade, I am a software developer who is very very skillfull @ his job. I have worked on over 50 thousand projects.. I am also a Blogger and a Youtuber. To know more about me visit me on legitsub.com/incrediblenetworks
+        </p>
+      </section3>
+
+      <div className='section4'>
+        <div className='getInTouch'>
+
+          <div>
+            <h5><b>Get in Touch</b></h5>
+          </div>
+
+          <p>
+          Hi guys my name is Ikeoluwa Ashade, I am  a software developer who is very very  skillfull @ his job. I have worked on  over 50 thousand projects.. I am also a  Blogger and a Youtuber. To know more   about me visit me on legitsub.com/  incrediblenetworks
+          </p>
+
+          <div>
+            <button>Subscribe</button>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+      
+      
+    </div>
   )
 }
 
