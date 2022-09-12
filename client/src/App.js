06c7e032ce534a939cd7f70ca54fc368455cdebd
Maudlin-Maris <chinazamaudlin@gmail.com>
2022-09-12 18:54:06 +0100

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
 import { Route, Routes} from "react-router-dom"
-// import AboutUs from "./components/AboutUs";
-// import ContactUs from "./components/ContactUs";
-// import Doctor from "./components/Doctor";
-// import Error from "./components/Error";
-// import Home from "./components/Home";
-// import Services from "./components/Services";
-// import SharedLayout from "./components/SharedLayout";
+import AboutUs from "./components/AboutUs";
+import ContactUs from "./components/ContactUs";
+import Doctor from "./components/Doctor";
+import Error from "./components/Error";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import SharedLayout from "./components/SharedLayout";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
 function App() {
   return (
     <div>
+      
       <Routes>
-        <Route path='Login' element={<Login/>}/>
-        <Route path='Signup' element={<Signup/>}/>
-
-      </Routes>
-      {/* <Routes>
       <Route path='/' element={<SharedLayout/>}>
       <Route index element={<Home />}/>
       <Route path="*" element={<Error/>}/>
@@ -25,9 +21,10 @@ function App() {
       <Route path="/service" element={<Services/>}/>
       <Route path="/about" element={<AboutUs/>}/>
       <Route path="/contact" element={<ContactUs/>}/>
-      <Route path="/register" element={<Register/>}/>
+      <Route path='Login' element={<Login/>}/>
+      <Route path='Signup' element={<Signup/>}/>
       </Route>
-      </Routes> */}
+      </Routes>
     </div>
   );
 }
